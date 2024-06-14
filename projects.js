8692a6b44c06d5cebb61b80b2c1b87f0a8b0b2b5
lana <pr0pro@mail.ru>
2024-06-14 16:24:03 +0300

    add project web

---
+++ projects.js
@@ -0,0 +1,49 @@
+const projects = [
+    {
+        title: 'Calculator',
+        languages: 'Python using third-party libraries',
+        description: 'Web calculator that uses only basic arithmetic operations. \
+            The project shows how well the specialist knows how to use algorithms.',
+        image: 'calculator_project.jpg'
+    },
+    {
+        title: 'Polynomial regression for graphs and calculations',
+        languages: 'C++ using third-party libraries',
+        description: 'A program that calculates the polynomial regression for any number \
+        of points and shows them on a graph.',
+        image: 'polynomial_project.png'
+    },
+    {
+        title: '3-d model of the globe',
+        languages: 'Python using third-party libraries',
+        description: 'A project that presents a 3-d model of the globe with an ellipse for \
+        the minimum radius of the satellite. The code takes into account the gravity and rotation of the earth.',
+        image: 'orbit_project.jpg'
+    }
+];
+
+let currentProject = 0;
+
+document.getElementById('prev').addEventListener('click', () => {
+    currentProject = (currentProject > 0) ? currentProject - 1 : projects.length - 1;
+    displayProject();
+});
+
+document.getElementById('next').addEventListener('click', () => {
+    currentProject = (currentProject < projects.length - 1) ? currentProject + 1 : 0;
+    displayProject();
+});
+
+function showProject(index) {
+    currentProject = index;
+    displayProject();
+}
+
+function displayProject() {
+    document.getElementById('project-title').innerText = projects[currentProject].title;
+    document.getElementById('project-languages').innerText = projects[currentProject].languages;
+    document.getElementById('project-description').innerText = projects[currentProject].description;
+    document.getElementById('project-image').src = projects[currentProject].image;
+}
+
+displayProject();
