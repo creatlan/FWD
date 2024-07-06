043a177a7158b3152a8244220fb7a08044bdca68
lana <pr0pro@mail.ru>
2024-07-06 22:50:54 +0300

    deploy process

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+    trailingSlash: true,
+  };
+  
